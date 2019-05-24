e8df4822b0bd18030ca943bde251eec1df6a4c13
AlaaDawabah <alaadawabah@gmail.com>
2019-05-24 05:55:49 +0200

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragAnDdrop';
+  todos = [{
+    name : 'React',
+    category : 'web Development'
+  }, {
+    name : 'Ionic',
+    category : 'Mobile Development'
+  }, {
+    name : 'Node js',
+    category : 'BackEnd Developement'
+  }];
+
+  completed = [{
+    name : 'Angular7',
+    category : 'FrontEnd Developmnet'
+  }, {
+    name : 'Sass',
+    category : 'Stylying'
+  }];
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray (event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 }
